3324b42ae50e88ee39c26a111ff9ecc7740fcbc8
Somers Matthews <somersbmatthews@gmail.com>
2019-12-11 22:08:45 -0600

    completed 1.3

---
+++ golang-1.2.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var x int
+var y string
+var z bool
+
+func main () {
+	fmt.Println(x, "is the value for x")
+	fmt.Println(y, "is the value for y")
+	fmt.Println(z, "is the value for z")
+}
