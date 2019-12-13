983673cfdf3296f5c2ae6f61e7795118ca6eac2f
Somers Matthews <somersbmatthews@gmail.com>
2019-12-12 20:47:25 -0600

    completed 1.5

--- 1.5.go
+++ 1.5.go
@@ -11,5 +11,9 @@ func main() {
 	fmt.Println(x)
 	fmt.Printf("%T\n", x)
 	x = 42
-	fmt.Println(x)
+	fmt.Println(x, "is value for x")
+	y = int(x)
+	fmt.Printf("%T", y)
+	fmt.Println(" is type of y")
+	fmt.Println(y, "is value for y")
 }
