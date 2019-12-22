9e9ee8c0c46f23cba6f5c1d975745d56684348f5
Somers Matthews <somersbmatthews@gmail.com>
2019-12-22 15:24:09 -0600

    finished 2.5

---
+++ 2.5.go
@@ -0,0 +1,23 @@
+/*
+Hands-on exercise #5
+Create a variable of type string using a raw string literal. Print it.
+solution: 
+*/
+
+package main
+
+import ( 
+	"fmt"
+)
+
+func main () {
+	a := `here is something
+	as
+	a
+	raw
+	string
+	literal 
+	"you see"
+	another thing`
+	fmt.Println(a)
+}
