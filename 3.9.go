4f85e7b9fcae5c11ae3f367336013a2d5073e52e
Somers Matthews <somersbmatthews@gmail.com>
2019-12-23 10:35:14 -0600

    finished 3.9

---
+++ 3.9.go
@@ -0,0 +1,12 @@
+/*
+Hands-on exercise #9
+Create a program that uses a switch statement with the switch expression specified as a variable of TYPE string with the IDENTIFIER “favSport”.
+solution:
+*/
+
+
+package main
+
+import (
+	"fmt"
+)
