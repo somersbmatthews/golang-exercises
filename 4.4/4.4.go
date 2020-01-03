2e99a31c2ac298ef24c365dc716b82e398e5190f
Somers Matthews <somersbmatthews@gmail.com>
2020-01-03 03:54:25 -0600

    at point of no tests to be run error

---
+++ 4.4/4.4.go
@@ -0,0 +1,35 @@
+/*
+Follow these steps:
+start with this slice
+x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+append to that slice this value
+52
+print out the slice
+in ONE STATEMENT append to that slice these values
+53
+54
+55
+print out the slice
+append to the slice this slice
+y := []int{56, 57, 58, 59, 60}
+print out the slice
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+}
+
+func AppendSlice(s []int) []int {
+	y := append(s, 52)
+	fmt.Println(y)
+
+	y = append(y, 53, 54, 55)
+	fmt.Println(y)
+
+	y = append(y, 56, 57, 58, 59, 60)
+	fmt.Println(y)
+	return y
+}
---
+++ 4.4/4.4_test.go
@@ -0,0 +1,17 @@
+package main_test
+
+import (
+	"golang-exercises/4.4"
+	"reflect"
+	"testing"
+)
+
+func AppendSliceTest(t *testing.T) {
+	x := []int{42, 43, 44, 45, 46, 47, 48, 50, 51}
+	got := main.AppendSlice(x)
+	want := []int{42, 43, 44, 45, 46, 47, 48, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Error("error!")
+	}
+}
