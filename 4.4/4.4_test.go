c76862dd87386e3b5ecc4be146c68a378d8793f1
Somers Matthews <somersbmatthews@gmail.com>
2020-01-03 04:03:59 -0600

    finished 4.4

--- 4.4/4.4_test.go
+++ 4.4/4.4_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func AppendSliceTest(t *testing.T) {
+func TestAppendSlice(t *testing.T) {
 	x := []int{42, 43, 44, 45, 46, 47, 48, 50, 51}
 	got := main.AppendSlice(x)
 	want := []int{42, 43, 44, 45, 46, 47, 48, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60}
