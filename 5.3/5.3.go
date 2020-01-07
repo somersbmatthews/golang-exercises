562cdc60ad870ede560475fc7fff514515dce02d
Somers Matthews <somersbmatthews@gmail.com>
2020-01-06 23:52:17 -0600

    finished 6.2, no tests

---
+++ 5.3/5.3.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+)
+
+type vehicle struct {
+	doors int
+	color string
+}
+
+type truck struct {
+	vehicle
+	fourWheel bool
+}
+
+type sedan struct {
+	vehicle
+	luxury bool
+}
+
+func main() {
+	StructMaker()
+}
+
+func StructMaker() {
+	t := truck{
+		vehicle: vehicle{
+			doors: 2,
+			color: "white",
+		},
+		fourWheel: true,
+	}
+
+	s := sedan{
+		vehicle: vehicle{
+			doors: 4,
+			color: "black",
+		},
+		luxury: false,
+	}
+	fmt.Println(t)
+	fmt.Println(s)
+	fmt.Println(t.doors)
+	fmt.Println(s.doors)
+}
---
+++ 5.3/5.3_test.go
@@ -0,0 +1,14 @@
+package main_test
+
+import (
+	"golang-exercises/5.3"
+)
+
+func ExampleStructMaker() {
+	main.StructMaker()
+	// Output:
+	// {{2 white} true}
+	// {{4 black} false}
+	// 2
+	// 4
+}
