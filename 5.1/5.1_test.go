b83ca10da660e9c010f8ee454ff91c0b49df2842
Somers Matthews <somersbmatthews@gmail.com>
2020-01-06 18:19:01 -0600

    finished 5.2

---
+++ 5.1/5.1_test.go
@@ -0,0 +1,20 @@
+package main_test
+
+import (
+	"golang-exercises/5.1"
+)
+
+func ExampleMapMaker() {
+	main.MapMaker()	
+	// Output:
+	// James
+	// Bond
+	// 0 chocolate
+	// 1 martini
+	// 2 rum and coke
+	// Miss
+	// Moneypenny
+	// 0 strawberry
+	// 1 vanilla
+	// 2 capuccino	
+}
