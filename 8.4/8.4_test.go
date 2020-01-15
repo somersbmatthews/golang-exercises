a4b97bd0cf44080da822b086d3a8fba9aa9f640c
Somers Matthews <somersbmatthews@gmail.com>
2020-01-15 12:43:13 -0600

    finished 8.4

---
+++ 8.4/8.4_test.go
@@ -0,0 +1,12 @@
+package main
+
+import ()
+
+func ExampleSort() {
+	Sort()
+	// Output:
+	// [5 8 2 43 17 987 14 12 21 1 4 2 3 93 13]
+	// [1 2 2 3 4 5 8 12 13 14 17 21 43 93 987]
+	// [random rainbow delights in torpedo summers under gallantry fragmented moons across magenta]
+	// [across delights fragmented gallantry in magenta moons rainbow random summers torpedo under]
+}
