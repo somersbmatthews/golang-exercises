304475ae0370c52021432b6dda466ee53790bf2a
Somers Matthews <somersbmatthews@gmail.com>
2020-01-16 15:49:03 -0600

    finished 8.5

---
+++ 8.5/8.5_test.go
@@ -0,0 +1,46 @@
+package main
+
+import ()
+
+func ExampleMakeText() {
+	MakeText()
+	// Output:
+	// James Bond 32
+	// 	In his majesty's royal service
+	// 	Shaken, not stirred
+	// 	Youth is no guarantee of innovation
+	// Miss Moneypenny 27
+	// 	I would really prefer to be a secret agent myself.
+	// 	James, it is soo good to see you
+	// 	Would you like me to take care of that for you, James?
+	// M Hmmmm 54
+	// 	Can someone please tell me where James Bond is?
+	// 	Dear God, what has James done now?
+	// 	Oh, James. You didn't.
+	// -------
+	// Miss Moneypenny 27
+	// 	I would really prefer to be a secret agent myself.
+	// 	James, it is soo good to see you
+	// 	Would you like me to take care of that for you, James?
+	// James Bond 32
+	// 	In his majesty's royal service
+	// 	Shaken, not stirred
+	// 	Youth is no guarantee of innovation
+	// M Hmmmm 54
+	// 	Can someone please tell me where James Bond is?
+	// 	Dear God, what has James done now?
+	// 	Oh, James. You didn't.
+	// -------
+	// James Bond 32
+	// 	In his majesty's royal service
+	// 	Shaken, not stirred
+	// 	Youth is no guarantee of innovation
+	// M Hmmmm 54
+	// 	Can someone please tell me where James Bond is?
+	// 	Dear God, what has James done now?
+	// 	Oh, James. You didn't.
+	// Miss Moneypenny 27
+	// 	I would really prefer to be a secret agent myself.
+	// 	James, it is soo good to see you
+	// 	Would you like me to take care of that for you, James?
+}
